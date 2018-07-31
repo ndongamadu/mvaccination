efad02a21e283bf8b65018b017b7c0d981b7184a
ndongamadu <ndongamadou@gmail.com>
2018-07-31 12:17:29 +0000

    templating

--- functional_tests/test_functional.py
+++ functional_tests/test_functional.py
@@ -1,21 +1,25 @@
 from selenium import webdriver 
 import unittest 
-
+from django.urls import reverse
+from django.contrib.staticfiles.testing import LiveServerTestCase
 
 class NewVisitorTest(unittest.TestCase):
 	"""docstring for NewVisitorTest"""
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		#self.browser.implicitly_wait(3)
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
-		self.browser.quit(2)
+		self.browser.quit()
 
-	def test_everything_is_working(self):
-		self.browser.get('http://localhost:8000')
+	# def get_full_url(self, namespace):
+	# 	return self.live_server_url + reverse(namespace)
+
+	def test_work_title(self):
+		self.browser.get("http:localhost:8000")
 
 		#testing just if django is well launched
-		self.assertIn('Django', self.browser.title)
+		self.assertIn('Welcome To | Bootstrap', self.browser.title)
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
